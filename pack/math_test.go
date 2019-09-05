7647c2ca35c5d2869d56279c036a01653ce9fe44
Trey Fletcher <treyfletcher612@gmail.com>
2019-09-05 10:39:32 -0400

    working on testing in Go

---
+++ pack/math_test.go
@@ -0,0 +1,17 @@
+package pack
+
+import "testing"
+
+func TestCanAddNumbers(t *testing.T) {
+	result := Add(1, 2)
+
+	if result != 3 {
+		t.Log("failed to add one and two")
+		t.Fail()
+	}
+
+	result = Add(1, 2, 3, 4)
+	if result != 10 {
+		t.Error("Failed to add more than two numbers")
+	}
+}
